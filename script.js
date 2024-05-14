f4f145218a21460e4179bc602f9d043b499d051a
priyanka Mukherjee <priyankamukherjee@priyankas-MacBook-Air.local>
2024-05-14 23:54:05 +0530

    update script

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function openTab(pageURL) {
-    // Get all tab elements
-    var tabs = document.querySelectorAll("nav ul li");
-    // Remove active class from all tabs
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove("active");
-    }
-
-    // Add active class to the selected tab
-    event.target.classList.add("active");
-
-    // Load the respective HTML page for the selected tab
-    document.getElementById("tabContent").innerHTML = "";
-    if (pageURL !== "") {
-        window.location.href = pageURL;
-    }
+function togglemenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    console.log("hello");
 }
